e230b8717e808e315e244a7fb4195fe84f70741a
sarikamohan08 <sarikamohan08@gmail.com>
2022-06-05 17:11:19 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name = 'AutoClassifier',
   packages = find_packages(),
   include_package_data=True,
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Automatic Image classification ',
   author = 'Sarika Mohanraj',
